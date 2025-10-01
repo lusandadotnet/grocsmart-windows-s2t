7c0a9dcfb353a6fc42e9d1397c638bb1efe2b6e4
Sakhile Artwell Khumalo <emskaykayson@gmail.com>
2025-10-01 22:16:34 +0200

    updated the Show Password Button.

--- ont-design/LoginForm.cs
+++ ont-design/LoginForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,7 @@ namespace ont_design
         public LoginForm()
         {
             InitializeComponent();
+            passwordTextBox.UseSystemPasswordChar = true;
         }
 
         private void linkLabelToQuitApp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
